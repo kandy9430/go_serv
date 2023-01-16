d0a1f7f913204bbb9dcc0a4090b1a6911fc9c39f
Andrew Keane <kandy9495@gmail.com>
2023-01-15 21:03:41 -0800

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kandy9430/go_serv
+
+go 1.18
---
+++ server.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	var dir string
+
+	if len(os.Args) > 1 {
+		dir = os.Args[1]
+	} else {
+		dir, _ = os.Getwd()
+	}
+	fmt.Print(dir)
+	// liveServer := http.FileServer(dir)
+	// fmt.Println("Listening on port 8080")
+	// log.Fatal(http.ListenAndServe(":8080", liveServer))
+}
