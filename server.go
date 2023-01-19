e570b94274d91ac07879ea6c47735faf0c8f2a5c
Andrew Keane <kandy9495@gmail.com>
2023-01-18 16:58:57 -0800

    Kill server after changes to files

--- server.go
+++ server.go
@@ -1,11 +1,34 @@
 package main
 
 import (
-	"fmt"
+	"io/fs"
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
+	"time"
 )
+
+func watchFile(filePath string, changed chan bool) {
+	log.Printf("watching: %s\n", filePath)
+	initialStat, err := os.Stat(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		stat, err := os.Stat(filePath)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if initialStat.ModTime() != stat.ModTime() && initialStat.Size() != stat.Size() {
+			break
+		}
+		time.Sleep(1 * time.Second)
+	}
+	changed <- true
+}
 
 func main() {
 	var dir string
@@ -16,6 +39,30 @@ func main() {
 		dir, _ = os.Getwd()
 	}
 	liveServer := http.FileServer(http.Dir(dir))
-	fmt.Println("Listening on port 8080")
-	log.Fatal(http.ListenAndServe(":8080", liveServer))
+	log.Println("Listening on port 8080")
+
+	go func() { log.Fatal(http.ListenAndServe(":8080", liveServer)) }()
+
+	doneChan := make(chan bool)
+
+	go func(doneChan chan bool) {
+		err := filepath.WalkDir(dir, func(path string, d fs.DirEntry, err error) error {
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			go watchFile(path, doneChan)
+
+			return nil
+		})
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// <-changed
+	}(doneChan)
+
+	<-doneChan
+	log.Println("Restarting due to changes")
 }
