c752cbffa2cd7cc5872514078bce70f2b0fcfa38
Andrew Keane <kandy9495@gmail.com>
2023-02-09 19:21:35 -0800

    Fix bugs after merge, ignore dotfiles for restart

--- server.go
+++ server.go
@@ -1,6 +1,6 @@
-// failing when a file is removed from directory
-// stops the server, and then fails when trying to restart
-// 	probably something to do with the next call to startServer()
+// dotfiles are ignored, but it will still restart when a new dotfile is created or removed
+// should probably also add graceful shutdown
+// need to set up FileServer to ignore dotfiles. This is in the docs
 package main
 
 import (
@@ -9,10 +9,14 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"path"
 	"path/filepath"
+	"strings"
 	"time"
 )
 
+// creates a simple server
+// would like to add functionality to ignore .dotfiles
 func createServer() *http.Server {
 	return &http.Server{
 		Addr:    ":8080",
@@ -20,17 +24,21 @@ func createServer() *http.Server {
 	}
 }
 
+// watches an individual file for changes.
+// gets initial file Stat, and then compares that with current file Stat after 1 second
+// if the file changes, changed channel will fill
+// which will unblock call to Shutdown() in startServer()
 func watchFile(filePath string, changed chan bool) {
-	log.Printf("watching: %s\n", filePath)
 	initialStat, err := os.Stat(filePath)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Error with initial file stat: %v\n", err)
 	}
 
 	for {
 		stat, err := os.Stat(filePath)
 		if err != nil {
-			log.Fatal(err)
+			log.Printf("Error with file stat: %v\n", err)
+			break
 		}
 
 		if initialStat.ModTime() != stat.ModTime() && initialStat.Size() != stat.Size() {
@@ -41,13 +49,20 @@ func watchFile(filePath string, changed chan bool) {
 	changed <- true
 }
 
-func addWatchers(dir string, c chan bool) {
-	err := filepath.WalkDir(dir, func(path string, d fs.DirEntry, err error) error {
+// adds watchers for reach file in the directory
+// c is used to pass the doneChan from startServer() to each fileWatcher
+func addWatchers(c chan bool) {
+	dir := os.Getenv("DIR")
+	err := filepath.WalkDir(dir, func(name string, d fs.DirEntry, err error) error {
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		go watchFile(path, c)
+		if isDotFile(path.Base(name)) {
+			return nil
+		}
+
+		go watchFile(name, c)
 
 		return nil
 	})
@@ -55,40 +70,30 @@ func addWatchers(dir string, c chan bool) {
 	if err != nil {
 		log.Fatal(err)
 	}
+	log.Printf("Watching directory: %v\n", dir)
 }
 
-// func startServer(dir string) {
-// 	srv := createServer()
-//
-// 	log.Println("Listening on port 8080")
-//
-// 	doneChan := make(chan bool)
-//
-// 	go func() {
-// 		if err := server.ListenAndServe(); err != nil {
-// 			log.Fatal(err)
-// 		}
-// 	}()
-//
-// 	go addWatchers(dir, doneChan)
-// 	<-doneChan
-//
-// 	log.Println("Restarting due to changes")
-// }
+// checks if file is a dotfile. Expects to be passed the base name (path.Base(filename))
+func isDotFile(name string) bool {
+	if strings.HasPrefix(name, ".") {
+		return true
+	}
+	return false
+}
 
+// creates a new server and calls ListenAndServe()
+// sets up watchers for the directory
+// calls Shutdown() on server if files change
+// recursive call to startServer() to restart the server
+// need to add graceful shutdown
 func startServer() {
 	srv := createServer()
 	log.Printf("Listening on port %v\n", srv.Addr)
 
 	idleConnsClosed := make(chan struct{})
 	go func() {
-		// stop := make(chan os.Signal, 1)
-		// signal.Notify(stop, os.Interrupt)
-
-		// <-stop
-
 		doneChan := make(chan bool)
-		go addWatchers(dir, doneChan)
+		go addWatchers(doneChan)
 		<-doneChan
 
 		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
@@ -109,22 +114,6 @@ func startServer() {
 	startServer()
 }
 
-// func watchForInterrupt(srv *http.Server) {
-// 	stop := make(chan os.Signal, 1)
-// 	signal.Notify(stop, os.Interrupt)
-//
-// 	<-stop
-//
-// 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-// 	defer cancel()
-//
-// 	if err := srv.Shutdown(ctx); err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	log.Println("Server restarted")
-// 	// true ->c
-// }
-
 func main() {
 	var dir string
 	if len(os.Args) > 1 {
@@ -133,6 +122,7 @@ func main() {
 		dir, _ = os.Getwd()
 	}
 
+	// add directory to watch to ENV to be accessed easily by other functions
 	if err := os.Setenv("DIR", dir); err != nil {
 		log.Fatal(err)
 	}
